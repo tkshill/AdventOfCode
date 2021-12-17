a3724dfddc3925c83f002d4d4e946313f089b957
Kirk Shillingford <tkshillinz@icloud.com>
2021-12-16 23:31:19 -0400

    Update day12.fsx

--- day12.fsx
+++ day12.fsx
@@ -1,11 +1,15 @@
 open System.IO
 open System.Collections.Generic
 
+// ----------------- SETUP -----------------------
+
 type Cave =
     | Start
     | End
     | Big of string
     | Small of string
+
+type CaveMap = Dictionary<Cave, list<Cave>>
     
 let toPair (str: string) = str.Split "-" |> Seq.pairwise |> Seq.head
 
@@ -20,20 +24,22 @@ let rawData =
     File.ReadAllLines "day12.txt"
     |> Seq.map (toPair >> (fun (s1, s2) -> toCave s1, toCave s2))
 
-let update (d:Dictionary<Cave, list<Cave>>) s1 s2 =
+let update (d:CaveMap) s1 s2 =
     if d.ContainsKey s1 then d[ s1 ] <- s2 :: d[ s1 ]
     else d.Add(s1, [ s2 ])
     
-let d = Dictionary<Cave, Cave list>()
+let d = CaveMap()
     
 Seq.iter (fun (s1, s2) -> update d s1 s2; update d s2 s1) rawData
-    
+
+// --------------- PART ONE ----------------------------
+
 let isValidProgression history = function
     | Start -> false
     | Small c when Seq.exists ((=) (Small c)) history -> false
     | _ -> true
  
-let rec recurser1 (d:Dictionary<Cave, list<Cave>>) history key =
+let rec recurser1 (d:CaveMap) history key =
      if key = End then 1
      else
          let nextSet = Seq.filter (isValidProgression history) d[key]
@@ -44,8 +50,8 @@ let rec recurser1 (d:Dictionary<Cave, list<Cave>>) history key =
              
 let day12part1solution = recurser1 d [] Start
 
-// ---------------------------------------------------------------------
-// PART 2
+// ---------------------------------- PART 2-----------------------------------
+
 
 let small = function | Small _ -> true | _ -> false
 
@@ -61,7 +67,7 @@ let updateRepeatAllowed key history repeatAllowed =
     if small key && Seq.exists ((=) key) history then false
     else repeatAllowed
     
-let rec recurser2 (d:Dictionary<Cave, list<Cave>>) repeatAllowed history key =
+let rec recurser2 (d:CaveMap) repeatAllowed history key =
     if key = End then 1
     else
         let newRp = updateRepeatAllowed key history repeatAllowed
